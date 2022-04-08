66dab1c6f0bd084c0feefe369c8a66aa4d3b3913
dstntLghts <ctzeranis@gmail.com>
2022-04-08 18:32:09 +0300

    Deck difficulty added, Meny class added

--- mydeck.py
+++ mydeck.py
@@ -1,4 +1,6 @@
 import random
+import tkinter as tk
+import tkinter.ttk as ttk
 
 class Card:
     def __init__(self,suit,value):
@@ -10,38 +12,89 @@ class Card:
         print("{}{}".format(self.value,self.suit))
 
 class Deck:
-    def __init__(self):
+    def __init__(self,difficulty):
+        self.difficulty=difficulty
         self.cards=[] 
-        self.build()
+        self.build(self.difficulty)
         self.shuffle()
 
-    def build(self):
-        for s in ["♠","♣","♦","♥"]:
-            for v in (1,2,3,4,5,6,7,8,9,10,'J','Q','K'):
-                self.cards.append(Card(s,v))
+    def build(self,diff_choice):
+
+        if diff_choice=='easy':
+            for s in ["♠","♣","♦","♥"]:
+                for v in (10,'J','Q','K'):
+                    self.cards.append(Card(s,v))
+        elif diff_choice=='average':
+            for s in ["♠","♣","♦","♥"]:
+                for v in (1,2,3,4,5,6,7,8,9,10):
+                    self.cards.append(Card(s,v))
+        elif diff_choice=='hard':
+            for s in ["♠","♣","♦","♥"]:
+                for v in (1,2,3,4,5,6,7,8,9,10,'J','Q','K'):
+                    self.cards.append(Card(s,v))
     
     def shuffle(self):
         for i in range(len(self.cards)-1,0,-1):
             r=random.randint(0,i)
             self.cards[i],self.cards[r]=self.cards[r],self.cards[i]
-        
+    
     def show(self):
         for c in self.cards:
             c.show()
 
     def select_cards(card_choice):
         pass
-        # Πατημα Κουμπιου
-        # Επιλογη 1ης Καρτας
-        # Επιλογη 2ης Καρτας
-        # Έλεγχος αν ειναι ιδιες
-        # Αν ΝΑΙ: Παραμενουν ανοιχτες και ποντος στον αντιστοιχο παικτη
-        # Αν ΟΧΙ: Κλείνουνε και παιζει ο επομενος παικτης
+# Πατημα Κουμπιου
+# Επιλογη 1ης Καρτας
+# Επιλογη 2ης Καρτας
+# Έλεγχος αν ειναι ιδιες
+# Αν ΝΑΙ: Παραμενουν ανοιχτες και ποντος στον αντιστοιχο παικτη
+# Αν ΟΧΙ: Κλείνουνε και παιζει ο επομενος παικτης
   
 class Player:
     def __init__(self,name,score=0):
         self.name = name
         self.score = score
+        
+class Menu:
+     def __init__(self):
+         pass
 
-test = Deck()
+     def play():
+         pass
+    
+class GUI:
+
+    def __init__(self):
+        self.root = tk.Tk()
+        self.build()
+
+    def build(self):
+        play_btn = ttk.Button(self.root,text="Play",command=menu.play)
+        quit_btn = ttk.Button(self.root,text="Quit",command=self.root.destroy)
+        play_btn.pack()
+        quit_btn.pack()
+        self.root.mainloop()
+
+    def play(self):
+        print("Game Started")
+
+    def select_players(self):
+        pass
+
+    def quit(self):
+        print("Quiting game")
+
+    def save_game(self):
+        pass
+
+    def load_game(self):
+        pass
+
+    def difficulty(self):
+        pass
+
+test = Deck("hard")
 test.show()
+menu = Menu()
+interface = GUI()
