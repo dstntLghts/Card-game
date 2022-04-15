7d867948fa1e68f684f04c6576a98dbe28f4bbef
dstntLghts <ctzeranis@gmail.com>
2022-04-16 00:16:34 +0300

    player and diff selection v1

--- mydeck.py
+++ mydeck.py
@@ -1,6 +1,6 @@
 import random
 from unicodedata import name
-import numpy as np
+#import numpy as np
 import tkinter as tk
 import tkinter.ttk as ttk
 
@@ -9,7 +9,7 @@ class Card:
         self.suit=suit
         self.value=value
         self.id=id
-        self.open= True # Ανοιχτή/Κλειστή
+        self.open= False # Ανοιχτή/Κλειστή
 
     def show(self):
         if self.open==False:
@@ -20,26 +20,36 @@ class Card:
 class Deck:
     def __init__(self,difficulty):
         self.cards=[]
+        self.cards_check = []
+        self.counter = 0
+        self.rows=0
+        self.cols=0
         self.build(difficulty)
         self.shuffle()
 
     def build(self,diff_choice):
         id=0
-        if diff_choice=='easy':    
+        if diff_choice=='easy':
             for s in ["♠","♣","♦","♥"]:
-                for v in (10,'J','Q','K'):
+                for v in ("10",'J','Q','K'):
                     id=id+1
                     self.cards.append(Card(s,v,id))
+            self.rows=4
+            self.cols=4
         elif diff_choice=='average':
             for s in ["♠","♣","♦","♥"]:
-                for v in (1,2,3,4,5,6,7,8,9,10):
+                for v in ("1","2","3","4","5","6","7","8","9","10"):
                     id=id+1
                     self.cards.append(Card(s,v,id))
+            self.rows=4
+            self.cols=10
         elif diff_choice=='hard':
             for s in ["♠","♣","♦","♥"]:
-                for v in (1,2,3,4,5,6,7,8,9,10,'J','Q','K'):
+                for v in ("1","2","3","4","5","6","7","8","9","10",'J','Q','K'):
                     id=id+1
                     self.cards.append(Card(s,v,id))
+            self.rows=4
+            self.cols=13
 
     def shuffle(self):
         for i in range(len(self.cards)-1,0,-1):
@@ -48,7 +58,7 @@ class Deck:
 
     def show_deck(self):
         for c in self.cards:
-                c.show()
+            c.show()
 
     def select_cards(card_choice):
         pass
@@ -60,108 +70,41 @@ class Deck:
 # Αν ΟΧΙ: Κλείνουνε και παιζει ο επομενος παικτης
 
 class Player:
-    def __init__(self,name,score=0):
+    def __init__(self,name,score=0,myturn=0):
         self.name = name
         self.score = score
+        self.myturn=myturn
 
 
 class GUI:
 
     def __init__(self):
-        self.root = tk.Tk()
-        self.root.geometry("400x300") # Μεγεθος παραθυρου
-        self.root.resizable(0,0) # Κανει το παραθυρο να μην επεκτεινεται
-        self.cards = []
-        self.build()
+        self.ndeck = None
+        self.build_menu()
 
-    def build(self):
+    def build_menu(self):
+        self.root = tk.Tk()
+        self.root.geometry("400x300+550+250") # Μεγεθος+θεση παραθυρου
+        self.root.resizable(0,0) # Κανει το παραθυρο να μην επεκτεινεται
         self.root.title("Cards Game")  # Τιτλος παραθυρου
         style = ttk.Style() # Δημιουργια Στυλ
         style.theme_use('alt')
         style.configure('A.TButton', font=('Helvetica',12,),  background='#8cd921')  #
         # Κουμπια
-        play_btn = ttk.Button(self.root,text="Play",command=self.play,style='A.TButton')
-        slc_players_btn = ttk.Button(self.root,text="Select Players",command=self.select_players,style='A.TButton')
+        load_btn = ttk.Button(self.root,text="Load game",command=self.load_game,style='A.TButton')
+        difficulty_btn = ttk.Button(self.root,text="New game",command=self.difficulty,style='A.TButton')
         quit_btn = ttk.Button(self.root,text="Quit",command=self.root.destroy,style='A.TButton')
         # Τοποθετηση Κουμπιων
-        play_btn.grid(row=0,column=0,ipadx=50,ipady=20,padx=90,pady=10)
-        slc_players_btn.grid(row=1,column=0,ipadx=50,ipady=20,padx=90,pady=10)
+        load_btn.grid(row=0,column=0,ipadx=50,ipady=20,padx=90,pady=10)
+        difficulty_btn.grid(row=1,column=0,ipadx=50,ipady=20,padx=90,pady=10)
         quit_btn.grid(row=2,column=0,ipadx=50,ipady=20,padx=90,pady=10)
         # Εκκινηση παραθυρου
         self.root.mainloop()
 
-    def play(self):
-        print("Game Started")
-        self.root.destroy()  # Διαγραφη παραθυρου μενου
-        self.root = tk.Tk()  # Εκκινηση παραθυρου παιχνιδιου
-        self.root.resizable(0,0)
-        card_id = 0
-        style = ttk.Style()
-        style.theme_use('alt')
-        style.configure('A.TButton', font=('Helvetica',12,), foreground="red")
-        style.configure('B.TButton', font=('Helvetica',12,), foreground="black")
-        style.configure('Hidden.TButton',font=('Helvetica',20))
-        frame = ttk.Frame(width=300,height=300)
-        for i in range(4):
-            for j in range (4):
-                # Test Drive Cards
-                # if test.cards[card_id].suit in ["♦","♥"]:
-                #     card = ttk.Button(self.root,text=f"{test.cards[card_id].value}{test.cards[card_id].suit}",style="A.TButton")
-                # else:
-                #     card = ttk.Button(self.root,text=f"{test.cards[card_id].value}{test.cards[card_id].suit}",style="B.TButton")
-                card = tk.Button(self.root,text="🂠",height=3,width=5,font=("Helvetica"),command=lambda i=i,j=j:self.select_card(i,j))
-                # remove i=i j=j to see late binding issue
-                # lamda για περασμα arguments
-                card.grid(row=i,column=j)
-                card_id += 1
-        self.root.mainloop()
-
-
-    def select_card(self,row,col):
-        # κωδικας για επιλογη καρτας
-        print(row,col)
-
-    def select_players(self):
-        #  Under construction
-        self.root.destroy()  # Διαγραφη παραθυρου μενου
-        self.root = tk.Tk()  # Εκκινηση παραθυρου παιχνιδιου
-        self.root.geometry("300x400")
-        self.root.resizable(0,0)
-        self.root.title("Cards Game")  # Τιτλος παραθυρου
-        style = ttk.Style() # Δημιουργια Στυλ
-        style.theme_use('alt')
-        style.configure('A.TButton', font=('Helvetica',12,),  background='#111111')
-        player1 = ttk.Button(self.root, text="1 Player", command=self.action_select_players)
-        player2 = ttk.Button(self.root, text="2 Players", command=lambda i=2:self.action_select_players(i))
-        player3 = ttk.Button(self.root, text="3 Players", command=self.action_select_players)
-        player4 = ttk.Button(self.root, text="4 Players", command=self.action_select_players)
-        player1.grid(row=0,column=0,ipadx=50,ipady=20,padx=63,pady=10)
-        player2.grid(row=1,column=0,ipadx=50,ipady=20,padx=30,pady=10)
-        player3.grid(row=2,column=0,ipadx=50,ipady=20,padx=30,pady=10)
-        player4.grid(row=3,column=0,ipadx=50,ipady=20,padx=30,pady=10)
-        self.root.mainloop()
-
-    def action_select_players(self,num):
-        #  Under construction [CT] Passed arg from players 2 button, to create two Players and then initiate game. 
-        if num==2:
-            player1=Player("Player 1")
-            player2=Player("Player 2")
-            self.difficulty()
-        # to be cont.
-
-    def quit(self):
-        print("Quiting game")
-
-    def save_game(self):
-        pass
-
-    def load_game(self):
-        pass
-
     def difficulty(self): # after Player number next windows could be diff choice following the same manner.
         self.root.destroy()  # Διαγραφη παραθυρου μενου
         self.root = tk.Tk()  # Εκκινηση παραθυρου παιχνιδιου
-        self.root.geometry("300x400")
+        self.root.geometry("300x300+550+250")
         self.root.resizable(0,0)
         self.root.title("Cards Game")  # Τιτλος παραθυρου
         style = ttk.Style() # Δημιουργια Στυλ
@@ -175,8 +118,109 @@ class GUI:
         hard.grid(row=2,column=0,ipadx=50,ipady=20,padx=30,pady=10)
         self.root.mainloop()
 
-    def create_deck(self,mode): #creates a shuffled deck depending users choice 
-        ndeck=Deck(mode)
-        ndeck.show_deck()
+    def create_deck(self,mode): #creates a shuffled deck depending users choice
+        self.ndeck=Deck(mode)
+        self.select_players()
 
+    def play(self):
+        print("Game Started")
+        self.root = tk.Tk()  # Εκκινηση παραθυρου παιχνιδιου
+        self.root.title("Cards Game")
+        self.root.resizable(0,0)
+        self.root.geometry("+550+250")
+        save_btn = ttk.Button(self.root,text="Save Game",command=self.save_game,style='A.TButton')
+        card_id = 0
+        for i in range(self.ndeck.rows):
+            for j in range (self.ndeck.cols):
+                card = tk.Button(self.root,text="🂠",height=3,width=5,font=("Helvetica"),
+                                 command=lambda card_id=card_id,i=i,j=j:self.select_card(card_id,i,j))
+                # late binding issue
+                # using lamda to pass arguments
+                
+                card.grid(row=i,column=j)
+                card_id += 1
+        save_btn.grid(row=(self.ndeck.rows+1),column=(self.ndeck.cols),ipadx=0,ipady=0,padx=5,pady=5)
+        self.root.mainloop()
+
+
+    def select_card(self,card_id,r,c):
+        # κωδικας για επιλογη καρτας
+        if self.ndeck.cards[card_id].suit in ["♦","♥"]: # Adds red color
+            card = tk.Label(self.root,text=f"{self.ndeck.cards[card_id].value}{self.ndeck.cards[card_id].suit}",
+                            height=3,width=5,font=("Helvetica"),fg="red")
+        else:
+            card = tk.Label(self.root,text=f"{self.ndeck.cards[card_id].value}{self.ndeck.cards[card_id].suit}",
+                            height=3,width=5,font=("Helvetica"))
+        card.grid(row=r,column=c)
+        self.ndeck.cards[card_id].open = True
+        self.ndeck.cards_check.append(list((self.ndeck.cards[card_id],card_id,r,c)))
+        print(self.ndeck.cards_check)
+        self.ndeck.counter += 1
+        if self.ndeck.counter == 2:
+            after(1000,self.evaluate_cards())
+
+
+    def evaluate_cards(self):
+        # Κωδικας για τσεκαρισμα αν ο παικτης εχει σκοραρει
+        # ΠΡΕΠΕΙ ΝΑ ΒΑΛΟΥΜΕ DELAY με το tk.after()
+        print("ENTERED CHEK")
+        if self.ndeck.cards_check[0][0].value != self.ndeck.cards_check[1][0].value:
+            print("MUST HIDE")
+            card_1 = tk.Button(self.root,text="🂠",height=3,width=5,font=("Helvetica"),
+                             command=lambda card_id=self.ndeck.cards_check[0][1],
+                                            r=self.ndeck.cards_check[0][2],
+                                            c=self.ndeck.cards_check[0][3]: self.select_card(card_id,r,c))
+            card_2 = tk.Button(self.root,text="🂠",height=3,width=5,font=("Helvetica"),
+                             command=lambda card_id=self.ndeck.cards_check[1][1],
+                                            r=self.ndeck.cards_check[1][2],
+                                            c=self.ndeck.cards_check[1][3]: self.select_card(card_id,r,c))
+            card_1.grid(row=self.ndeck.cards_check[0][2],column=self.ndeck.cards_check[0][3])
+            card_2.grid(row=self.ndeck.cards_check[1][2],column=self.ndeck.cards_check[1][3])
+        self.ndeck.counter = 0
+        self.ndeck.cards_check.clear()
+
+    def select_players(self):
+        #  Under construction
+        self.root.destroy()  # Διαγραφη παραθυρου μενου
+        self.root = tk.Tk()  # Εκκινηση παραθυρου παιχνιδιου
+        self.root.geometry("300x400+550+250")  # Μεγεθος+θεση παραθυρου
+        self.root.resizable(0,0)
+        self.root.title("Cards Game")  # Τιτλος παραθυρου
+        style = ttk.Style()  # Δημιουργια Στυλ
+        style.theme_use('alt')
+        style.configure('A.TButton', font=('Helvetica',12,),  background='#111111')
+        player1 = ttk.Button(self.root, text="1 Player", command=lambda i=1:self.action_select_players(i))
+        player2 = ttk.Button(self.root, text="2 Players", command=lambda i=2:self.action_select_players(i))
+        player3 = ttk.Button(self.root, text="3 Players", command=lambda i=3:self.action_select_players(i))
+        player4 = ttk.Button(self.root, text="4 Players", command=lambda i=4:self.action_select_players(i))
+        player1.grid(row=0,column=0,ipadx=50,ipady=20,padx=63,pady=10)
+        player2.grid(row=1,column=0,ipadx=50,ipady=20,padx=30,pady=10)
+        player3.grid(row=2,column=0,ipadx=50,ipady=20,padx=30,pady=10)
+        player4.grid(row=3,column=0,ipadx=50,ipady=20,padx=30,pady=10)
+        self.root.mainloop()
+
+    def action_select_players(self,num):
+        players.clear()  # Remove previous players
+        for i in range(1,5):  # Adds Players
+            if i == num:
+                for j in range(i):
+                    players.append(Player(f"Player{j}"))
+                break
+        print(players)
+        self.root.destroy()
+        self.play()
+
+    def quit(self):
+        print("Quiting game")
+
+    def save_game(self):
+        pass
+
+    def load_game(self):
+        pass
+
+players = []
 interface = GUI()
+
+
+# Code DUMP (don't delete may use later)
