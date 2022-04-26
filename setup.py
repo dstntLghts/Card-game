4b2e752e447df8e56a72cd00c0f0f628c9c21a2f
thanos7282 <thanos7282@gmail.com>
2022-04-26 20:56:00 +0300

    Completed Wildcards "JJ,KK" Added setup file Minor GUI updates

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Project05',
+    version='',
+    packages=[''],
+    url='https://github.com/dstntLghts/ILE-66',
+    license='',
+    author='Project-Team4',
+    author_email='',
+    description=''
+)
